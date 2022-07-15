744ee927d4c96b4c1565960a6a71f3b430c49a3f
MCWZ <3210103579@zju.edu.cn>
2022-07-15 18:53:45 +0800

    disc01

---
+++ discs/disc01.py
@@ -0,0 +1,30 @@
+# 1.1
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    if temp >= 60 and raining == False:
+        return False
+    else:
+        return True
+
+def wears_jacket(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    return 1
+#1.2
+#overflow?
+#1.3
+#easy is_prime()
+#1.4
